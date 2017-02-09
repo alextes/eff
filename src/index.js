d43151ee541ab29052cf2216b2e128ca970803a1
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-02-09 12:04:11 +0100

    Report port we're listening on when ready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 require('babel-polyfill');
+const pino = require('pino')();
 const server = require('./server.js');
 
-server.listen(8080);
+const PORT = 8080;
+server.listen(PORT);
+pino.info(`server listening on ${PORT}`);
