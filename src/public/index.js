af0effc404f91d46503674621477e1508bc38db8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-02-10 22:16:22 +0100

    Update client script to pull vids
    Pulls the video's list from the server and enables the get-feels
    button when ready.

--- src/public/index.js
+++ src/public/index.js
@@ -1,5 +1,52 @@
-const [player] = document.getElementsByClassName('feels-player');
+let vids = [];
 
-// TODO: get next vid url
-player.setAttribute('src', 'vid/1484502893.webm');
-player.play();
+/**
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    /* eslint-disable no-param-reassign */
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+    /* eslint-enable no-param-reassign */
+  }
+
+  return array;
+}
+
+// TODO: play next button should be disabled until this completes
+fetch('vid-list.json')
+  .then(response => response.blob())
+  .then((vidListJSON) => {
+    vids = JSON.parse(vidListJSON);
+    shuffle(vids);
+
+    const [button] = document.getElementsByClassName('get-feels');
+    button.removeAttribute('disabled');
+  });
+
+function startNextVid() {
+  const [player] = document.getElementsByClassName('feels-player');
+  const nextVid = vids.pop();
+
+  // TODO: get next vid url
+  player.setAttribute('src', `vid/${nextVid}`);
+  player.play();
+}
+
+window.onload = () => {
+  const [button] = document.getElementsByClassName('get-feels');
+  button.onclick = startNextVid;
+};
