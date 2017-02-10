01a5e5c731d41fccf1a6c4bc74af2ba6e382a735
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-02-10 22:36:53 +0100

    Build the vid list on server start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 require('babel-polyfill');
 const pino = require('pino')();
+const { buildList } = require('./build-vid-list.js');
 const server = require('./server.js');
 
+// build the video's list
+buildList();
+
+// start the server
 const PORT = 8080;
 server.listen(PORT);
 pino.info(`server listening on ${PORT}`);
